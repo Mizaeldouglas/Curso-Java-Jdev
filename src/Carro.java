37364b05b57bec2bb64ed31b8e6234d86fce88cf
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-05-16 00:14:19 -0300

    first commit

---
+++ src/Carro.java
@@ -0,0 +1,15 @@
+
+public class Carro {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		int velocidade;
+		String cor;
+		String marca;
+		boolean importado;
+		double valor;
+
+	}
+
+}
